7e66cb0812f733c2629dc9e4e3d47fa58dd9a092
amina <aminasaedd@gmail>
2019-04-04 16:41:34 +0300

    Updates

---
+++ precache-manifest.711c77fb1b2d345be58b4865bca4271e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "112eb2a9bc0338f97a28",
+    "url": "/ContactManager/static/js/runtime~main.0e09fb07.js"
+  },
+  {
+    "revision": "6049791263e2845e9c06",
+    "url": "/ContactManager/static/js/main.ed4ce6ff.chunk.js"
+  },
+  {
+    "revision": "049b23cc06f7f35b2609",
+    "url": "/ContactManager/static/js/2.20d82ee2.chunk.js"
+  },
+  {
+    "revision": "6049791263e2845e9c06",
+    "url": "/ContactManager/static/css/main.92b891b7.chunk.css"
+  },
+  {
+    "revision": "049b23cc06f7f35b2609",
+    "url": "/ContactManager/static/css/2.e7eef20a.chunk.css"
+  },
+  {
+    "revision": "4f79e17cd3d9a06b2ab1394f28123888",
+    "url": "/ContactManager/index.html"
+  }
+];
